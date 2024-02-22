a62d0f50aa2fb23617db580e47d16658ece0efe6
Ba Thien Tran <bathientran@gmail.com>
2024-02-22 07:11:21 -0800

    chore(): replacing old supabase implementation with new supabase clients

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,19 +1,17 @@
-import Login from "@/app/_components/Login";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import { login, signup } from "./actions";
 
-export default async function LoginPage() {
-  const supabase = createServerComponentClient({ cookies });
-  const { data } = await supabase.auth.getSession();
-  if (data.session?.user) {
-    redirect("/");
-  }
-
+export default function LoginPage() {
   return (
-    <main className="max-w-lg m-auto">
+    <>
       <h1 className="text-2xl text-center mb-6">Login</h1>
-      <Login />
-    </main>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form>
+    </>
   );
 }
