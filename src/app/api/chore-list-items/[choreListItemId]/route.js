776af1ac6a365cd40df889b309023c9c7495337e
Ba Thien Tran <bathientran@gmail.com>
2024-02-24 19:15:54 -0800

    chore(): adapting old chore endpoint

---
+++ src/app/api/chore-list-items/[choreListItemId]/route.js
@@ -0,0 +1,196 @@
+import { NextResponse } from "next/server";
+import { getProfileIfMember } from "../../_utils";
+
+export const dynamic = "force-dynamic";
+
+/**
+ * @swagger
+ * /api/chore-list-items/[choreListItemId]:
+ *   patch:
+ *     tag:
+ *      - Chore List Items
+ *     summary: Update a choreListItem
+ *     description: Update a choreListItem
+ *     parameters:
+ *       - in: path
+ *         name: choreListItemId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the choreListItem
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/choreListItem'
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *   delete:
+ *     tag:
+ *      - Chore List Items
+ *     summary: Delete a choreListItem
+ *     description: Delete a choreListItem
+ *     parameters:
+ *       - in: path
+ *         name: choreListItemId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the choreListItem
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/choreListItem'
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *
+ * components:
+ *    schemas:
+ *      choreListItem:
+ *        type: object
+ *        properties:
+ *          id:
+ *            type: string
+ *          name:
+ *            type: string
+ *          status:
+ *           type: string
+ *          createdAt:
+ *            type: string
+ *          updatedAt:
+ *            type: string
+ *          assignedToId:
+ *            type: string
+ *          createdById:
+ *            type: string
+ *      Error:
+ *        type: object
+ *        properties:
+ *          message:
+ *            type: string
+ */
+export async function PATCH(request, context) {
+  const data = await request.json();
+  try {
+    const choreListItemId = context.params.choreListItemId;
+
+    const choreListItem = await prisma.choreListItem.findUnique({
+      where: {
+        id: choreListItemId,
+      },
+      include: {
+        choreList: true,
+      },
+    });
+
+    if (!choreListItem) {
+      return NextResponse.json({ message: "Item not found" }, { status: 404 });
+    }
+
+    const profile = await getProfileIfMember(choreListItem.choreList.roomId);
+
+    if (!profile) {
+      return NextResponse.json(
+        { message: "User is not a member of the room" },
+        { status: 400 }
+      );
+    }
+
+    const updatechoreListItem = await prisma.choreListItem.update({
+      where: {
+        id: choreListItemId,
+      },
+      data: {
+        ...data,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "Updated item",
+        choreListItem: updatechoreListItem,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error updating item", error: error },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request, context) {
+  try {
+    const choreListItemId = context.params.choreListItemId;
+
+    const choreListItem = await prisma.choreListItem.findUnique({
+      where: {
+        id: choreListItemId,
+      },
+      include: {
+        choreList: true,
+      },
+    });
+
+    if (!choreListItem) {
+      return NextResponse.json({ message: "Item not found" }, { status: 404 });
+    }
+
+    const profile = await getProfileIfMember(choreListItem.choreList.roomId);
+
+    if (!profile) {
+      return NextResponse.json(
+        { message: "User is not a member of the room" },
+        { status: 400 }
+      );
+    }
+
+    const deletechoreListItem = await prisma.choreListItem.update({
+      where: {
+        id: choreListItemId,
+      },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "Deleted item",
+        choreListItem: deletechoreListItem,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error deleting item", error: error },
+      { status: 500 }
+    );
+  }
+}
