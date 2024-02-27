55f2a595d5917c9f60321120d43bd1009a28894b
Chiamaka U <chiamakabrowneyes@Chiamakas-MBP.lan>
2024-02-27 15:21:38 -0800

    help review the blank contact me section

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
     <main>
-      <h1 className="text-2xl text-center mb-8">Protected page</h1>
+      <h1 className="text-2xl text-center mb-8"></h1>
     </main>
   );
 }
