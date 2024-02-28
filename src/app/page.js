0e6161069995810eb0639ec2575431917f7e8e5b
Ba Thien Tran <bathientran@gmail.com>
2024-02-27 20:26:36 -0800

    hotifx(): replacing home page screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
-export default async function Home() {
+import CreateRoomBox from "@/app/_components/CreateRoomBox/CreateRoomBox";
+import RoomsOverview from "@/app/_components/RoomsOverview";
+import TopBar from "@/app/_components/TopBar/TopBar";
+import { getProfile } from "@/utils/api";
+import "./globals.css";
+
+export default async function LandinPage() {
+  const user = await getProfile();
+
   return (
-    <main>
-      <h1 className="text-2xl text-center mb-8">Protected page</h1>
-    </main>
+    <div>
+      <TopBar title={"Rooms"} />
+      <div className="mainContainer">
+        <RoomsOverview rooms={user.rooms} />
+        <CreateRoomBox context={{ user: user }} />
+      </div>
+    </div>
   );
 }
