b378954dcbb0fad18326fdc084ceef115f1b748b
Waiyego-Wangari <waiyegowangari@gmail.com>
2024-03-04 18:59:59 +0000

    added classes to components

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,16 +3,34 @@ import "./page.css";
 
 export default function LoginPage() {
   return (
-    <>
+    <div className="loginPage">
+      {" "}
+      {/* Added class to apply page styles */}
       <h1 className="text-2xl text-center mb-6">Login</h1>
-      <form>
+      <form id="loginForm">
         <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" required />
+        <input
+          id="email"
+          name="email"
+          type="email"
+          required
+          className="loginInput"
+        />
         <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" required />
-        <button formAction={login}>Log in</button>
-        <button formAction={signup}>Sign up</button>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          required
+          className="loginInput"
+        />
+        <button className="loginButton" formAction={login}>
+          Log in
+        </button>
+        <button className="loginButton" formAction={signup}>
+          Sign up
+        </button>
       </form>
-    </>
+    </div>
   );
 }
