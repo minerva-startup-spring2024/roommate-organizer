552bfae929a65ce0f96972b81f6da1f9f657f738
Ba Thien Tran <bathientran@gmail.com>
2024-03-07 09:40:56 -0800

    chore(): adds color variables

---
+++ src/app/_constants/colors.js
@@ -0,0 +1,8 @@
+export const colorGrey100 = "#DDDCE1";
+export const colorGrey300 = "#6F727D";
+export const colorGrey700 = "#373943";
+export const colorPinkAccent = "#DD5E89";
+export const colorPinkAction = "#F26C6D";
+export const colorPinkBackground = "#EDD4D4";
+export const colorWhiteBase = "#FFFFFF";
+export const colorBlackBase = "#000000";
