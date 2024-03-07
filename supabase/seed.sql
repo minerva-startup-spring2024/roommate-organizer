85b52e30bece27f536835fdb737282a2f058a08b
Ba Thien Tran <bathientran@gmail.com>
2024-03-07 08:47:54 -0800

    chore(): adding row level policy to storage object upon seeding

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -2,3 +2,5 @@ insert into
 storage.buckets (id, name, public, owner, avif_autodetection)
 values
 ('avatars', 'avatars', true, null, false);
+
+CREATE POLICY "insert_objects 1oj01fe_0" ON storage.objects FOR INSERT TO authenticated WITH CHECK (bucket_id = 'avatars');
