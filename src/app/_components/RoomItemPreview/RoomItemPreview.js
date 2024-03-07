9aae31a4b45726d9a22b4de235e654dca151689b
Ba Thien Tran <bathientran@gmail.com>
2024-03-07 13:37:48 -0800

    chore(): updates preview routes to link to respective subpage

--- src/app/_components/RoomItemPreview/RoomItemPreview.js
+++ src/app/_components/RoomItemPreview/RoomItemPreview.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FaAngleRight } from "react-icons/fa6";
 import styles from "./RoomItemPreview.module.css";
@@ -5,7 +6,7 @@ import styles from "./RoomItemPreview.module.css";
 const RoomItemPreview = ({ previewTitle, items, roomId }) => {
   return (
     <div className={styles.itemPreviewContainer}>
-      <p className={styles.boxTitle}>{previewTitle.toUpperCase()}</p>
+      <p className={styles.boxTitle}>{previewTitle}</p>
       <div className={styles.itemsContainer}>
         {items.length === 0 ? (
           <div className={styles.emptyText}>No {previewTitle}</div>
@@ -13,17 +14,32 @@ const RoomItemPreview = ({ previewTitle, items, roomId }) => {
           <>
             {items.slice(0, 3).map((item) => (
               <Link
-                href={`/rooms/${previewTitle}`}
+                href={`${roomId}/${previewTitle}`}
                 key={item.id}
                 className={styles.itemContainer}
               >
-                <div>{item.name}</div>
+                <div className={styles.itemLabel}>
+                  <Image
+                    src={
+                      item.assignedTo
+                        ? `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${item.assignedTo.profileImage}`
+                        : "/default.png"
+                    }
+                    alt={
+                      item.assignedTo ? item.assignedTo.profileImage : "No alt"
+                    }
+                    width={25}
+                    height={25}
+                    className={styles.itemImage}
+                  />
+                  {item.name}
+                </div>
                 <FaAngleRight color={"#373943"} />
               </Link>
             ))}
             <div className={styles.viewMoreContainer}>
               <Link
-                href={`/rooms/${previewTitle}/${roomId}`}
+                href={`${roomId}/${previewTitle}`}
                 className={styles.viewMoreButton}
               >
                 View all {previewTitle}
