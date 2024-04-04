8bf765a753647a4f9d7559dc036c18074c995c41
Vicki Petrova <90992029+vickipetrova@users.noreply.github.com>
2024-04-04 20:46:25 +0100

    Unified the migrations files into one singel migration

--- prisma/migrations/20240404194540_buildings_feature_changes/migration.sql
+++ prisma/migrations/20240404194540_buildings_feature_changes/migration.sql
@@ -1,7 +1,6 @@
 /*
   Warnings:
 
-  - You are about to drop the column `sentToId` on the `announcement` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[buildingId]` on the table `room` will be added. If there are existing duplicate values, this will fail.
   - Made the column `roomId` on table `announcement` required. This step will fail if there are existing NULL values in that column.
 
@@ -9,12 +8,8 @@
 -- DropForeignKey
 ALTER TABLE "public"."announcement" DROP CONSTRAINT "announcement_roomId_fkey";
 
--- DropForeignKey
-ALTER TABLE "public"."announcement" DROP CONSTRAINT "announcement_sentToId_fkey";
-
 -- AlterTable
-ALTER TABLE "public"."announcement" DROP COLUMN "sentToId",
-ALTER COLUMN "roomId" SET NOT NULL;
+ALTER TABLE "public"."announcement" ALTER COLUMN "roomId" SET NOT NULL;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "room_buildingId_key" ON "public"."room"("buildingId");
