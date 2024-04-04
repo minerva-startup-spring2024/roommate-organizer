fbcfd50065a5ed328b1eba49839ccc1ba32921f7
Vicki Petrova <90992029+vickipetrova@users.noreply.github.com>
2024-04-04 20:06:05 +0100

    Revert "remove all migrations from this branch"
    This reverts commit 18820e7a497917a42e1a9d8004a38d22ea1bb339.

---
+++ prisma/migrations/20240403010224_removed_unique_constriant_on_rooms_and_fixed_the_profile_schema_and_building_to_fix_it/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - Made the column `buildingId` on table `room` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."room" DROP CONSTRAINT "room_buildingId_fkey";
+
+-- DropIndex
+DROP INDEX "public"."room_buildingId_key";
+
+-- AlterTable
+ALTER TABLE "public"."room" ALTER COLUMN "buildingId" SET NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "public"."room" ADD CONSTRAINT "room_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "public"."building"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
