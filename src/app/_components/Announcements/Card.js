76358a74215e5288ee20d5b52d84a0407ae8359d
Chiamaka U <chiamakabrowneyes@Chiamakas-MBP.lan>
2024-04-05 13:00:03 -0700

    adding cards

--- src/app/_components/Announcements/Card.js
+++ src/app/_components/Announcements/Card.js
@@ -1,17 +1,15 @@
-"use client"
-import './Card.css';
+"use client";
+import "./Card.css";
 function Card(props) {
-    return (
-        <div className="card">
-            <img className="card-image" src={props.image} />
-            <h4> "From " + {props.fullName} </h4>
-            <br></br>
-            <p className="card-text">{props.text}</p>
-            <br></br>
-            
-            <h4> {props.date}</h4>
-        </div>
-    );
+  return (
+    <div className="card">
+      <img className="card-image" src={props.image} alt={props.altText} />
+      <h2 className="card-title">{props.title}</h2>
+      <br></br>
+      <p className="card-text">{props.text}</p>
+      <h4> {props.date}</h4>
+    </div>
+  );
 }
 
 export default Card;
