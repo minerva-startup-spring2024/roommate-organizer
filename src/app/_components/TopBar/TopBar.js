ed7d50b0e293915862e48ee885b6f05b6d07edcc
Waiyego-Wangari <waiyegowangari@gmail.com>
2024-04-07 20:57:34 +0100

    logout functionality and small UI changes

--- src/app/_components/TopBar/TopBar.js
+++ src/app/_components/TopBar/TopBar.js
@@ -1,8 +1,8 @@
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaSignOutAlt } from "react-icons/fa";
 import { FaGear } from "react-icons/fa6";
 import styles from "./TopBar.module.css";
 
-export default function TopBar({ title, details, onDetailsClick }) {
+export default function TopBar({ title, details, onDetailsClick, onLogout }) {
   return (
     <div className={styles.topBar}>
       <div className={styles.headerContainer}>
@@ -13,10 +13,12 @@ export default function TopBar({ title, details, onDetailsClick }) {
               <p className={styles.header}>{title}</p>
             </div>
             <FaGear size={22} color={"white"} />
+            <FaSignOutAlt size={22} color={"white"} onClick={onLogout} />
           </>
         ) : (
           <>
             <p className={styles.header}>{title}</p>
+            <FaSignOutAlt size={22} color={"white"} onClick={onLogout} />
           </>
         )}
       </div>
--- src/app/app/page.js
+++ src/app/app/page.js
@@ -3,13 +3,20 @@ import RoomsOverview from "@/app/_components/RoomsOverview.js";
 import TopBar from "@/app/_components/TopBar/TopBar.js";
 import { getProfile } from "@/app/api/_utils";
 import "@/app/globals.css";
+import { redirect } from "next/navigation";
 
 export default async function HomePage() {
   const user = await getProfile();
-
+  if (!user) {
+    redirect("/");
+  }
+  const handleLogout = async () => {
+    await fetch("/api/logout", { method: "POST" });
+    redirect("/"); // Redirect to login after calling logout endpoint
+  };
   return (
     <div>
-      <TopBar title={"Rooms"} />
+      <TopBar title={"Rooms"} onLogout={handleLogout} />
       <div className="mainContainer">
         <RoomsOverview rooms={user.rooms} />
         <CreateRoomBox context={{ user: user }} />
