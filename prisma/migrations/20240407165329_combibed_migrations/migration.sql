5728d4482b82f4bc8292cfe5df592582a0bd0555
richiewaweru <richie@minerva.kgi.edu>
2024-04-07 13:49:58 -0700

    add schema change to announcements and building

---
+++ prisma/migrations/20240407165329_combibed_migrations/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Made the column `buildingId` on table `room` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."room" DROP CONSTRAINT "room_buildingId_fkey";
+
+-- AlterTable
+ALTER TABLE "public"."room" ALTER COLUMN "buildingId" SET NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "public"."room" ADD CONSTRAINT "room_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "public"."building"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
