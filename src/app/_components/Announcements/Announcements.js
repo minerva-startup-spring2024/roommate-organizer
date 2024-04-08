cf492426f5d3d36410ba985ebc9ed9a55f256350
Ba Thien Tran <bathientran@gmail.com>
2024-04-08 16:41:54 -0700

    chore(announcements): refactors announcement implementation

---
+++ src/app/_components/Announcements/Announcements.js
@@ -0,0 +1,38 @@
+"use client";
+
+import Card from "@/app/_components/Announcements/Card";
+import { useEffect, useState } from "react";
+import CreatePost from "../CreatePost/CreatePost";
+
+const Announcements = ({ roomId }) => {
+  const [loading, setLoading] = useState(true);
+  const [announcements, setAnnouncements] = useState([]);
+
+  const fetchAnnouncements = async () => {
+    setLoading(true);
+    const response = await fetch(`/api/announcements?roomId=${roomId}`);
+    const data = await response.json();
+    setAnnouncements(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchAnnouncements();
+  }, []);
+
+  return (
+    <div>
+      <CreatePost roomId={roomId} fetchAnnouncements={fetchAnnouncements} />
+      {announcements.map((announcement, index) => (
+        <Card
+          key={index}
+          content={announcement.content}
+          user={announcement.sentBy}
+          updatedAt={announcement.updatedAt}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Announcements;
--- src/app/_components/Announcements/Card.js
+++ src/app/_components/Announcements/Card.js
@@ -1,15 +1,26 @@
 "use client";
-import "./Card.css";
-function Card(props) {
-  return (
-    <div className="card">
-      <img className="card-image" src={props.image} />
-      <h4> From {props.fullName} </h4>
-      <br></br>
-      <p className="card-text">{props.text}</p>
-      <br></br>
+import Image from "next/image";
+import styles from "./Card.module.css";
 
-      <h4> {props.date}</h4>
+function Card({ user, content, updatedAt }) {
+  return (
+    <div className={styles.card}>
+      <Image
+        src={
+          user
+            ? user.profileImage &&
+              `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${user.profileImage}`
+            : "/default.png"
+        }
+        alt={user ? user.profileImage && user.profileImage : "No alt"}
+        width={60}
+        height={60}
+        className={styles.profilePhoto}
+      />
+      <div className={styles.cardText}>{content}</div>
+      <div className={styles.dateText}>
+        {new Date(updatedAt).toLocaleString()}
+      </div>
     </div>
   );
 }
