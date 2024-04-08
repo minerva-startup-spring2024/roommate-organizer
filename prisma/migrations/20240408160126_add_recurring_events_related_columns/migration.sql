f69c51f40b7dfd8f349d40e339319b0be1d972c3
Vicki Petrova <90992029+vickipetrova@users.noreply.github.com>
2024-04-08 22:59:57 +0100

    hotfix(): attempt to fix db by manually changing the migration file, removed anything related to buildings since it shouldn't be here

--- prisma/migrations/20240408160126_add_recurring_events_related_columns/migration.sql
+++ prisma/migrations/20240408160126_add_recurring_events_related_columns/migration.sql
@@ -1,19 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `_BuildingMembers` table. If the table is not empty, all the data it contains will be lost.
-  - A unique constraint covering the columns `[buildingId]` on the table `room` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- DropForeignKey
-ALTER TABLE "public"."_BuildingMembers" DROP CONSTRAINT "_BuildingMembers_A_fkey";
-
--- DropForeignKey
-ALTER TABLE "public"."_BuildingMembers" DROP CONSTRAINT "_BuildingMembers_B_fkey";
-
--- AlterTable
-ALTER TABLE "public"."building" ADD COLUMN     "buildingOwnerId" TEXT;
-
 -- AlterTable
 ALTER TABLE "public"."event" ADD COLUMN     "endTimezone" TEXT,
 ADD COLUMN     "followingId" TEXT,
@@ -24,12 +8,3 @@ ADD COLUMN     "startTimezone" TEXT,
 ALTER COLUMN "description" SET DEFAULT '',
 ALTER COLUMN "location" DROP DEFAULT,
 ALTER COLUMN "category" DROP DEFAULT;
-
--- DropTable
-DROP TABLE "public"."_BuildingMembers";
-
--- CreateIndex
-CREATE UNIQUE INDEX "room_buildingId_key" ON "public"."room"("buildingId");
-
--- AddForeignKey
-ALTER TABLE "public"."building" ADD CONSTRAINT "building_buildingOwnerId_fkey" FOREIGN KEY ("buildingOwnerId") REFERENCES "public"."profile"("id") ON DELETE SET NULL ON UPDATE CASCADE;
