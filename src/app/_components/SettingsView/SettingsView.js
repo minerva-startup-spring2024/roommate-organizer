30e56ac4128f9ccd2f4d84f22d19ccf3e36f87c6
Ba Thien Tran <bathientran@gmail.com>
2024-04-08 16:54:12 -0700

    chore(feedback): removes empty page container

--- src/app/_components/SettingsView/SettingsView.js
+++ src/app/_components/SettingsView/SettingsView.js
@@ -2,15 +2,21 @@ import styles from "./SettingsView.module.css";
 
 export default function SettingsView() {
   return (
-    <div className={styles.pageContainer}>
-      <div>
-        <p className={styles.previewText}>More coming soon &#x1F680;...</p>
-        <div className={styles.contactContainer}>
-          <a href="mailto:roommateorganizer@gmail.com" className={styles.contactButton}>
-            <span role="img" aria-label="Email" className={styles.emailIcon}>&#x1F4E7;</span> Contact us via email
-          </a>
-        </div>
-        <div className={styles.buttonFooter}>We&apos;d love to hear your feedback!</div>
+    <div>
+      <p className={styles.previewText}>More coming soon &#x1F680;...</p>
+      <div className={styles.contactContainer}>
+        <a
+          href="mailto:roommateorganizer@gmail.com"
+          className={styles.contactButton}
+        >
+          <span role="img" aria-label="Email" className={styles.emailIcon}>
+            &#x1F4E7;
+          </span>{" "}
+          Contact us via email
+        </a>
+      </div>
+      <div className={styles.buttonFooter}>
+        We&apos;d love to hear your feedback!
       </div>
     </div>
   );
