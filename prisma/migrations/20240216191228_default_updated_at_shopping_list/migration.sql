35a788fd831284a319d3f37a958b078312e99520
Ba Thien Tran <bathientran@gmail.com>
2024-04-10 13:43:01 -0700

    hotfix(): fixes inconsistent migrationand schema setup

--- prisma/migrations/20240216191228_default_updated_at_shopping_list/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."profile" ALTER COLUMN "userId" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "public"."shopping_list" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
--- prisma/migrations/20240221221116_adding_role/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "public"."Role" AS ENUM ('admin', 'roommate', 'manager');
-
--- AlterTable
-ALTER TABLE "public"."profile" ADD COLUMN     "role" "public"."Role" NOT NULL DEFAULT 'roommate';
