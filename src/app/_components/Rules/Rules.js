0e8b181fdbefe3cd1414dd776b6456ea34ddec73
chiamakabrowneyes <chiamaka@uni.minerva.edu>
2024-04-13 20:35:38 -0700

    fixing error

--- src/app/_components/Rules/Rules.js
+++ src/app/_components/Rules/Rules.js
@@ -22,7 +22,7 @@ const Rules = (props) => {
           <div className="heading-bullet"></div>
           <span>{props.heading ? props.heading : ""}</span>
           <div className="heading-date">
-            {props.fromDate && props.toDate ? ${props.fromDate} - ${props.toDate} : ""}
+            {props.fromDate && props.toDate ? `${props.fromDate} - ${props.toDate}` : ""}
             {props.demoLink ? 
               <span style={{ marginLeft: '10px' }}>
                 {React.cloneElement(props.demoLink, { style: { color: 'white' } })}
@@ -88,7 +88,7 @@ const Rules = (props) => {
         />
         <div className="chore-description">
           <span className="Rules-description-text">
-          {submittedParagraphs.paragraph1  paragraph1}
+          {submittedParagraphs.paragraph1 || paragraph1}
           </span>
         </div>
       </div>
@@ -104,92 +104,93 @@ const Rules = (props) => {
         />
         <div className="chore-description">
           <span className="Rules-description-text">
-          {submittedParagraphs.paragraph2 paragraph2}
+          {submittedParagraphs.paragraph2|| paragraph2}
           </span>
         </div>
       </div>
     </div>,
+
     <div className="Rules-screen-container" key="visiting-rules">
-    <div className="chore-container">
-      <RulesHeading
-        heading={"Visiting Rules"}
-        subHeading={""}
-        fromDate={" "}
-        toDate={" "}
-      />
-      <div className="chore-description">
-        <span className="Rules-description-text">
-        {submittedParagraphs.paragraph3  paragraph3}
-        </span>
-      </div>
-    </div>
-  </div>,
-];
-
-const handleCarousal = (index) => {
-  setSelectedBulletIndex(index);
-};
-
-const getBullets = () => {
-  const bulletNames = ["Payment Instructions", "Residential Policies", "Visiting Rules"];
-  return bulletNames.map((name, index) => (
-    <div
-      onClick={() => handleCarousal(index)}
-      className={
-        index === selectedBulletIndex ? "bullet selected-bullet" : "bullet"
-      }
-      key={index}
-    >
-      <span className="bullet-label">{name}</span>
-    </div>
-  ));
-};
-
-
-const getRulesScreens = () => {
-  return RulesDetails[selectedBulletIndex];
-};
-
-return (
-  <div className="Rules-container screen-container fade-in" id={props.id  ""}>
-    <div className="Rules-content">
-      <div title={"Rules"} subHeading={"My formal Bio Details"} />
-      <div className="Rules-card">
-        <div className="Rules-bullets">
-          <div className="bullet-container">
-            <div className="bullet-icons"></div>
-            <div className="bullets">{getBullets()}</div>
-          </div>
+      <div className="chore-container">
+        <RulesHeading
+          heading={"Visiting Rules"}
+          subHeading={""}
+          fromDate={" "}
+          toDate={" "}
+        />
+        <div className="chore-description">
+          <span className="Rules-description-text">
+          {submittedParagraphs.paragraph3 || paragraph3}
+          </span>
         </div>
+      </div>
+    </div>,
+  ];
 
-        <div className="Rules-bullet-details">{getRulesScreens()}</div>
+  const handleCarousal = (index) => {
+    setSelectedBulletIndex(index);
+  };
+
+  const getBullets = () => {
+    const bulletNames = ["Payment Instructions", "Residential Policies", "Visiting Rules"];
+    return bulletNames.map((name, index) => (
+      <div
+        onClick={() => handleCarousal(index)}
+        className={
+          index === selectedBulletIndex ? "bullet selected-bullet" : "bullet"
+        }
+        key={index}
+      >
+        <span className="bullet-label">{name}</span>
+      </div>
+    ));
+  };
+  
+
+  const getRulesScreens = () => {
+    return RulesDetails[selectedBulletIndex];
+  };
+
+  return (
+    <div className="Rules-container screen-container fade-in" id={props.id || ""}>
+      <div className="Rules-content">
+        <div title={"Rules"} subHeading={"My formal Bio Details"} />
+        <div className="Rules-card">
+          <div className="Rules-bullets">
+            <div className="bullet-container">
+              <div className="bullet-icons"></div>
+              <div className="bullets">{getBullets()}</div>
+            </div>
+          </div>
+
+          <div className="Rules-bullet-details">{getRulesScreens()}</div>
+        </div>
+      </div>
+      <div className="textarea-container">
+        <textarea
+          className="textarea"
+          value={paragraph1}
+          onChange={(e) => handleParagraphChange(e, 1)}
+          placeholder="Enter Payment Instructions"
+        />
+        <textarea
+          className="textarea"
+          value={paragraph2}
+          onChange={(e) => handleParagraphChange(e, 2)}
+          placeholder="Enter Residential Policies"
+        />
+        <textarea
+          className="textarea"
+          value={paragraph3}
+          onChange={(e) => handleParagraphChange(e, 3)}
+          placeholder="Enter Visiting Rules"
+        />
+        <button onClick={handleSubmit} className="submit-button">
+          Submit
+        </button>
       </div>
     </div>
-    <div className="textarea-container">
-      <textarea
-        className="textarea"
-        value={paragraph1}
-        onChange={(e) => handleParagraphChange(e, 1)}
-        placeholder="Enter Payment Instructions"
-      />
-      <textarea
-        className="textarea"
-        value={paragraph2}
-        onChange={(e) => handleParagraphChange(e, 2)}
-        placeholder="Enter Residential Policies"
-      />
-      <textarea
-        className="textarea"
-        value={paragraph3}
-        onChange={(e) => handleParagraphChange(e, 3)}
-        placeholder="Enter Visiting Rules"
-      />
-      <button onClick={handleSubmit} className="submit-button">
-        Submit
-      </button>
-    </div>
-  </div>
-);
+  );
 };
 
 export default Rules;
